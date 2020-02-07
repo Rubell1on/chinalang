21729ccccb7e85ac81951e5066bba811855ef42f
ablaev.dmitry <dmitry.ablaev@metahouse.ru>
2020-02-07 14:20:24 +0700

    Испаравлены проблемы лендинга с респонсивом

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
 
-app.listen(3000, () => {
+app.listen(3000, '192.168.1.133', () => {
     console.log('Server started');
 })
 
