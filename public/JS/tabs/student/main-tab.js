6304b2dde7f25effe7ae7517cdf6f62857102ce5
rubell1on <dablaev@yandex.ru>
2020-03-13 23:46:55 +0300

    Фикс

--- public/JS/tabs/student/main-tab.js
+++ public/JS/tabs/student/main-tab.js
@@ -115,7 +115,7 @@ async function renderMain(user) {
             });
 
             return new ObjectWrapper('insta-block', [
-                new Label(['insta-block__label', 'header'], 'Читайте наш instaBlog'),
+                new Label(['insta-block__label', 'header'], 'Читайте наш InstaBlog'),
                 new ObjectWrapper('insta-scroll', images)
             ])
         }        
