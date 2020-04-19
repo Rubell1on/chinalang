b07e8bae5cb2f5e771b35fb598723ed8804d5962
ablaev.dmitry <dmitry.ablaev@metahouse.ru>
2020-04-20 03:14:59 +0700

    Рефакторинг

--- public/JS/react/JS/components/login-form/login-form.js
+++ public/JS/react/JS/components/login-form/login-form.js
@@ -27,58 +27,58 @@ export class LoginForm extends React.Component {
     });
   }
 
-  async onSubmit(e) {
+  onSubmit(e) {
     e.preventDefault();
     console.log(this);
     notificationController.success('Получилось!');
-    const res = await request.get('/login', this.state).catch(e => {
+    request.get('/login', this.state).then(res => {
+      if (res.status === 'success') {
+        const role = res.response.role;
+        auth.setData(res.response);
+        const courseRoute = role === 'student' ? 'main' : 'users';
+        location.href = `${location.origin}/dashboard/${courseRoute}`;
+      }
+    }).catch(e => {
       console.log(e);
       notificationController.error(e.error.responseText);
     });
-
-    if (res.status === 'success') {
-      const role = res.response.role;
-      auth.setData(res.response);
-      const courseRoute = role === 'student' ? 'main' : 'users';
-      location.href = `${location.origin}/dashboard/${courseRoute}`;
-    } else {
-      notificationController.error('Ошибка!');
-    }
   }
 
   render() {
     return (
       /*#__PURE__*/
-      //    <form ref={this.loginForm} className={`form ${this.props.className}`} >
-      //         <CustomInput key="email" type="email" label="E-mail" onChange={e => this.onInputChange(e, 'email')} required={true}/>
-      //         <CustomInput key="password" type="password" label="Пароль" onChange={e => this.onInputChange(e, 'password')} required={true}/>
-      //         <CustomButton type="submit" className="button_justified button_big button_color_red" value="Войти" />
-      //    </form>
       React.createElement("form", {
         ref: this.loginForm,
         className: `form ${this.props.className}`,
-        onSubmit: async e => await this.onSubmit(e)
+        onSubmit: this.onSubmit
       },
       /*#__PURE__*/
-      React.createElement("input", {
+      React.createElement(CustomInput, {
         key: "email",
         type: "email",
+        label: "E-mail",
         onChange: e => this.onInputChange(e, 'email'),
         required: true
       }),
       /*#__PURE__*/
-      React.createElement("input", {
+      React.createElement(CustomInput, {
         key: "password",
         type: "password",
+        label: "\u041F\u0430\u0440\u043E\u043B\u044C",
         onChange: e => this.onInputChange(e, 'password'),
         required: true
       }),
       /*#__PURE__*/
-      React.createElement("input", {
+      React.createElement(CustomButton, {
         type: "submit",
         className: "button_justified button_big button_color_red",
         value: "\u0412\u043E\u0439\u0442\u0438"
-      }))
+      })) //     <form ref={this.loginForm} className={`form ${this.props.className}`} onSubmit={this.onSubmit}>
+      //         <input key="email" type="email" onChange={e => this.onInputChange(e, 'email')} required={true}/>
+      //         <input key="password" type="password" onChange={e => this.onInputChange(e, 'password')} required={true}/>
+      //         <input type="submit" className="button_justified button_big button_color_red" value="Войти" />
+      //    </form>
+
     );
   }
 
