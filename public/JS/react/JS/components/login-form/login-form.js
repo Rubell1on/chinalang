631a896051f0e01a99c5c15c9d1e0469a980329c
ablaev.dmitry <dmitry.ablaev@metahouse.ru>
2020-04-20 03:06:49 +0700

    Хотфикс

--- public/JS/react/JS/components/login-form/login-form.js
+++ public/JS/react/JS/components/login-form/login-form.js
@@ -11,17 +11,15 @@ export class LoginForm extends React.Component {
     this.onInputChange = this.onInputChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
     this.onInputChange = this.onInputChange.bind(this);
-  }
+  } // componentDidMount() {
+  //     const node = this.loginForm.current;
+  //     node.addEventListener("submit", async(e) => await this.onSubmit(e));
+  // }
+  // componentWillUnmount() {
+  //     const node = this.loginForm.current;
+  //     node.removeEventListener("submit", async (e) => await this.onSubmit(e));
+  // }
 
-  componentDidMount() {
-    const node = this.loginForm.current;
-    node.addEventListener("submit", async e => await this.onSubmit(e));
-  }
-
-  componentWillUnmount() {
-    const node = this.loginForm.current;
-    node.removeEventListener("submit", async e => await this.onSubmit(e));
-  }
 
   onInputChange(event, field) {
     this.setState({
@@ -43,34 +41,40 @@ export class LoginForm extends React.Component {
       auth.setData(res.response);
       const courseRoute = role === 'student' ? 'main' : 'users';
       location.href = `${location.origin}/dashboard/${courseRoute}`;
+    } else {
+      notificationController.error('Ошибка!');
     }
   }
 
   render() {
     return (
       /*#__PURE__*/
+      //    <form ref={this.loginForm} className={`form ${this.props.className}`} >
+      //         <CustomInput key="email" type="email" label="E-mail" onChange={e => this.onInputChange(e, 'email')} required={true}/>
+      //         <CustomInput key="password" type="password" label="Пароль" onChange={e => this.onInputChange(e, 'password')} required={true}/>
+      //         <CustomButton type="submit" className="button_justified button_big button_color_red" value="Войти" />
+      //    </form>
       React.createElement("form", {
         ref: this.loginForm,
-        className: `form ${this.props.className}`
+        className: `form ${this.props.className}`,
+        onSubmit: async e => await this.onSubmit(e)
       },
       /*#__PURE__*/
-      React.createElement(CustomInput, {
+      React.createElement("input", {
         key: "email",
         type: "email",
-        label: "E-mail",
         onChange: e => this.onInputChange(e, 'email'),
         required: true
       }),
       /*#__PURE__*/
-      React.createElement(CustomInput, {
+      React.createElement("input", {
         key: "password",
         type: "password",
-        label: "\u041F\u0430\u0440\u043E\u043B\u044C",
         onChange: e => this.onInputChange(e, 'password'),
         required: true
       }),
       /*#__PURE__*/
-      React.createElement(CustomButton, {
+      React.createElement("input", {
         type: "submit",
         className: "button_justified button_big button_color_red",
         value: "\u0412\u043E\u0439\u0442\u0438"
--- public/JS/react/JSX/components/login-form/login-form.jsx
+++ public/JS/react/JSX/components/login-form/login-form.jsx
@@ -16,22 +16,22 @@ export class LoginForm extends React.Component {
         this.onInputChange = this.onInputChange.bind(this);
     }
 
-    componentDidMount() {
-        const node = this.loginForm.current;
-        node.addEventListener("submit", async(e) => await this.onSubmit(e));
-    }
+    // componentDidMount() {
+    //     const node = this.loginForm.current;
+    //     node.addEventListener("submit", async(e) => await this.onSubmit(e));
+    // }
 
-    componentWillUnmount() {
-        const node = this.loginForm.current;
-        node.removeEventListener("submit", async (e) => await this.onSubmit(e));
-    }
+    // componentWillUnmount() {
+    //     const node = this.loginForm.current;
+    //     node.removeEventListener("submit", async (e) => await this.onSubmit(e));
+    // }
 
     onInputChange (event, field) {
         this.setState({[field]: event.target.value});
     }
 
     async onSubmit(e) {
-        e.preventDefault();
+        e.preventDefault()
         console.log(this);
         notificationController.success('Получилось!');
         const res = await request.get('/login', this.state)
@@ -45,15 +45,22 @@ export class LoginForm extends React.Component {
 
             const courseRoute = role === 'student' ? 'main' : 'users';
             location.href = `${location.origin}/dashboard/${courseRoute}`;
+        } else {
+            notificationController.error('Ошибка!');
         }
     }
 
     render() {
         return (
-           <form ref={this.loginForm} className={`form ${this.props.className}`} >
-                <CustomInput key="email" type="email" label="E-mail" onChange={e => this.onInputChange(e, 'email')} required={true}/>
-                <CustomInput key="password" type="password" label="Пароль" onChange={e => this.onInputChange(e, 'password')} required={true}/>
-                <CustomButton type="submit" className="button_justified button_big button_color_red" value="Войти" />
+        //    <form ref={this.loginForm} className={`form ${this.props.className}`} >
+        //         <CustomInput key="email" type="email" label="E-mail" onChange={e => this.onInputChange(e, 'email')} required={true}/>
+        //         <CustomInput key="password" type="password" label="Пароль" onChange={e => this.onInputChange(e, 'password')} required={true}/>
+        //         <CustomButton type="submit" className="button_justified button_big button_color_red" value="Войти" />
+        //    </form>
+            <form ref={this.loginForm} className={`form ${this.props.className}`} onSubmit={async e => await this.onSubmit(e)}>
+                <input key="email" type="email" onChange={e => this.onInputChange(e, 'email')} required={true}/>
+                <input key="password" type="password" onChange={e => this.onInputChange(e, 'password')} required={true}/>
+                <input type="submit" className="button_justified button_big button_color_red" value="Войти" />
            </form>
         )
     }
